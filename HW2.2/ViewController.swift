dd3f103692a8e75f00480b69f23ff8a5c56c336c
Rusty <j3rry@ya.ru>
2022-06-19 22:11:27 +0300

    trafficLight

--- HW2.2/ViewController.swift
+++ HW2.2/ViewController.swift
@@ -7,13 +7,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var circleRed: UIView!
+    @IBOutlet weak var circleYellow: UIView!
+    @IBOutlet weak var circleGreen: UIView!
+    
+    @IBOutlet weak var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        circleRed.alpha = 0.3
+        circleYellow.alpha = 0.3
+        circleGreen.alpha = 0.3
+        
+        circleRed.layer.cornerRadius = circleRed.frame.width / 2
+        circleYellow.layer.cornerRadius = circleYellow.frame.width / 2
+        circleGreen.layer.cornerRadius = circleGreen.frame.width / 2
+        
+        startButton.layer.cornerRadius = 15
+
     }
 
-
+    
+    @IBAction func StartButtenTap() {
+        
+        startButton.setTitle("NEXT", for: .normal)
+    }
+    
 }
 
